d2f4e92437adf1b2e4cc1de62b8e9c1e209eaec9
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-12-04 11:39:16 +0100

    Test improved. The main modification concerns the test:  * correctESAICGradesTest which now is enabled to repeat the interaction with the LLM several times if the returned result is different from what expected. Also the test now leverages the factory defined with the previos commit

--- src/test/java/it/cnr/iasi/saks/llmEsaic/tests/LoadingESAICTest.java
+++ src/test/java/it/cnr/iasi/saks/llmEsaic/tests/LoadingESAICTest.java
@@ -24,8 +24,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.opentest4j.AssertionFailedError;
 
 import it.cnr.iasi.saks.llmEsaic.impl.DummyESAICPrompter;
+import it.cnr.iasi.saks.llmEsaic.utils.ESAICPrompterTestingFactory;
 
 public class LoadingESAICTest {
 
@@ -33,29 +35,53 @@ public class LoadingESAICTest {
 	
 	@BeforeAll
     public static void setup() {
-    	prompter = new DummyESAICPrompter();
+//    	prompter = new DummyESAICPrompter();
+    	prompter = ESAICPrompterTestingFactory.getInstance().getDummyPrompter();
     }
 	
     @ParameterizedTest
     @CsvSource({"1,1", "1,2", "1,3", "1,4", "10,1", "10,2", "11,1", "12,10", "12,11", "12,1", "12,2", "12,3", "12,4", "12,5", "12,6", "12,7", "12,8", "12,9", "2,1", "2,2", "3,1", "3,2", "3,3", "3,4", "3,5", "4,1", "4,2", "5,1", "5,2", "5,3", "6,1", "6,2", "6,3", "6,4", "6,5", "6,6", "7,1", "7,2", "8,1", "8,2", "8,3", "8,4", "8,5", "8,6", "8,7", "8,8", "8,9", "9,1", "9,2", "9,3"})
 //    @CsvSource({"1,1"})
     public void loadESAICTest(int picoNumber, int recNumber) {
+    	
     	assertTrue(prompter.loadESAIC(picoNumber, recNumber));
     }
 
     @ParameterizedTest
     @CsvSource({"1,5", "10,3", "11,2", "12,12", "2,3", "3,6", "4,3", "5,4", "6,7", "7,3", "8,10", "9,4", "144,33", "144,-33", "-144,33", "-144,-33",})
     public void loadWrongESAICTest(int picoNumber, int recNumber) {
-    	assertFalse(prompter.loadESAIC(picoNumber, recNumber));
+    	System.err.println("Processing ESAIC Recommendation: " + picoNumber + ", " + recNumber + " ...");
+    	boolean isLoaded = prompter.loadESAIC(picoNumber, recNumber);
+    	System.err.println("done ESAIC Recommendation: " + picoNumber + ", " + recNumber);
+    	
+    	assertFalse("Pico: " + picoNumber +", Rec: " + recNumber, isLoaded);
     }
 
 
     @ParameterizedTest
-    @CsvSource({"1,1,1C", "1,2,1B", "1,3,CPS", "1,4,CPS", "10,1,2B", "10,2,2C", "11,1,1C", "12,10,CPS", "12,11,CPS", "12,1,CPS", "12,2,CPS", "12,3,CPS", "12,4,CPS", "12,5,CPS", "12,6,CPS", "12,7,CPS", "12,8,CPS", "12,9,CPS", "2,1,CPS", "2,2,CPS", "3,1,2C", "3,2,2C", "3,3,1A", "3,4,1C", "3,5,1C", "4,1,2B", "4,2,2B", "5,1,1C", "5,2,1C", "5,3,CPS", "6,1,1C", "6,2,1A", "6,3,2C", "6,4,1C", "6,5,CPS", "6,6,1C", "7,1,1C", "7,2,2C", "8,1,2C", "8,2,1C", "8,3,1A", "8,4,2C", "8,5,2C", "8,6,2C", "8,7,2C", "8,8,2B", "8,9,1C", "9,1,1C", "9,2,1C", "9,3,1C"})
+//    @CsvSource({"1,1,1C", "1,2,1B", "1,3,CPS", "1,4,CPS", "10,1,2B", "10,2,2C", "11,1,1C", "12,10,CPS", "12,11,CPS", "12,1,CPS", "12,2,CPS", "12,3,CPS", "12,4,CPS", "12,5,CPS", "12,6,CPS", "12,7,CPS", "12,8,CPS", "12,9,CPS", "2,1,CPS", "2,2,CPS", "3,1,2C", "3,2,2C", "3,3,1A", "3,4,1C", "3,5,1C", "4,1,2B", "4,2,2B", "5,1,1C", "5,2,1C", "5,3,CPS", "6,1,1C", "6,2,1A", "6,3,2C", "6,4,1C", "6,5,CPS", "6,6,1C", "7,1,1C", "7,2,2C", "8,1,2C", "8,2,1C", "8,3,1A", "8,4,2C", "8,5,2C", "8,6,2C", "8,7,2C", "8,8,2B", "8,9,1C", "9,1,1C", "9,2,1C", "9,3,1C"})
+    @CsvSource({"1,1,1C", "1,2,1B", "1,3,3A", "1,4,3A", "10,1,2B", "10,2,2C", "11,1,1C", "12,10,3A", "12,11,3A", "12,1,3A", "12,2,3A", "12,3,3A", "12,4,3A", "12,5,3A", "12,6,3A", "12,7,3A", "12,8,3A", "12,9,3A", "2,1,3A", "2,2,3A", "3,1,2C", "3,2,2C", "3,3,1A", "3,4,1C", "3,5,1C", "4,1,2B", "4,2,2B", "5,1,1C", "5,2,1C", "5,3,3A", "6,1,1C", "6,2,1A", "6,3,2C", "6,4,1C", "6,5,3A", "6,6,1C", "7,1,1C", "7,2,2C", "8,1,2C", "8,2,1C", "8,3,1A", "8,4,2C", "8,5,2C", "8,6,2C", "8,7,2C", "8,8,2B", "8,9,1C", "9,1,1C", "9,2,1C", "9,3,1C"})
 //    @CsvSource({"1,1,GRADE: 1C"})
     public void correctESAICGradesTest(int picoNumber, int recNumber, String expectedGrade) {
-    	String response = prompter.queryRecommendationGrade(picoNumber, recNumber);
-    	assertEquals(expectedGrade, response, "Pico: " + picoNumber +", Rec: " + recNumber );
+    	int max_iterations = 5;
+    	int counter = 0;
+    	boolean completed = false;
+    	
+		System.err.println("Processing ESAIC Recommendation: " + picoNumber + ", " + recNumber + " ...");
+    	while (! completed) {
+    		String response = prompter.queryRecommendationGrade(picoNumber, recNumber);
+    		try {
+    			assertEquals(expectedGrade, response, "Pico: " + picoNumber +", Rec: " + recNumber );
+    			completed = true;
+    		} catch (AssertionFailedError e) {
+    			counter ++;
+    			System.err.println("Failed processing ESAIC Recommendation: " + picoNumber + ", " + recNumber + ". Tentative " + counter + " of " + max_iterations);
+    			if (counter >= max_iterations) {    				
+    				throw e;
+    			}
+    		}	
+    	}	
+		System.err.println("done ESAIC Recommendation: " + picoNumber + ", " + recNumber);
     }
 
   
